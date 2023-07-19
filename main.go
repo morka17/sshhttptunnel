0a467de066aa093e5ff242ba6620681edf2478a8
morka17 <joshuamorka19@gmail.com>
2023-07-19 01:47:36 +0100

    shh connection installed

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gliderlabs/ssh"
+)
+
+type Tunnel struct {
+	w      io.Writer
+	donech chan struct{}
+}
+
+var tunnels = map[int]chan Tunnel{}
+
+func main() {
+
+	go func() {
+		http.HandleFunc("/", handleRequest)
+		http.ListenAndServe(":3000", nil)
+	}()
+
+	ssh.Handle(func(s ssh.Session) {
+		id := rand.Intn(math.MaxInt)
+
+		tunnels[id] = make(chan Tunnel)
+
+		fmt.Println("tunnel ID -> ", id)
+		fmt.Println("tunnel is ready")
+
+		tunnel := <-tunnels[id]
+		_, err := io.Copy(tunnel.w, s)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		close(tunnel.donech)
+	})
+
+	log.Fatal(ssh.ListenAndServe(":2222", nil))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
+	id, _ := strconv.Atoi(idStr)
+
+	tunnel, ok := tunnels[id]
+	if !ok {
+		w.Write([]byte("tunnel not found"))
+		return 
+	}
+
+	donech := make(chan struct{})
+	tunnel <- Tunnel{
+		w: w, 
+		donech: donech,
+	}
+
+	<- donech
+}
